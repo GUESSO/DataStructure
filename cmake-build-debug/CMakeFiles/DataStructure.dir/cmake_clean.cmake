d304251ba938b4d6af3e59d4bdbeb1242e54225d
GUESSO <18401605721@163.com>
2017-12-28 19:51:36 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/DataStructure.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructure.dir/main.cpp.o"
+  "DataStructure.pdb"
+  "DataStructure.exe"
+  "libDataStructure.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataStructure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
